32e1f35bc9d68ebd77818ef6ebf4e9ebc6201aa6
Vincent Valenzuela <vincent.valenzuela@fundacion-jala.org>
2021-05-12 18:58:01 -0400

    Finished homepage and searchbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,70 @@
 import React, { useState, useEffect } from 'react'
 import './Home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Image } from "react-bootstrap"
 import MovieList from "./MovieList";
 import MovieListHeading from './MovieListHeading';
 import SearchBox from './SearchBox';
+import logo from '../assets/logo.png'
 
 const Home = () => {
 	const [movies, setMovies] = useState([]);
+	const [prevMovies, setPrevMovies] = useState([]);
     const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async () => {
-		const url = `https://mocki.io/v1/d7796acb-1cb4-4e09-8064-680b921d74bf `;
+		const url = `https://varplayerapi.herokuapp.com/movie`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
-
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
-		}
+		setPrevMovies(responseJson);
+		setMovies(responseJson);	
 	};
+	const searchMovie = () => {
+		let arraymovies = [];
+		for(var i = 0; i < movies.length; i++)
+		{
+		if(searchValue.length > 0)
+		{
+			if (movies[i].name.substr(0, searchValue.length).toUpperCase() == searchValue.toUpperCase()) 
+			{	
+				const foundMovie = movies[i];
+				arraymovies.push(foundMovie);
+			}
+		}
+		else
+		{
+			arraymovies = prevMovies;
+		}
+		}
+		if(arraymovies.length === 0)
+		{
+			arraymovies = prevMovies;	
+		}
+		else
+		{
+			setMovies(arraymovies);
+		}
 
-	useEffect(() => {
+		
+		
+
+	  }
+	  useEffect(() => {
 		getMovieRequest();
 	}, []);
 
+	useEffect(() => {
+		searchMovie(searchValue);
+	}, [searchValue]);
+
     return (
 		<div className='container-fluid movie-app'>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Movies' />
+			<Image src={logo} className="topleft"/>
+			<div className='row d-flex'>
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
-			<div className='row'>
+			<div className='movies d-flex'>
 				<MovieList movies={movies} />
 			</div>
 		</div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,9 +3,12 @@ import React from 'react';
 const MovieList = (props) => {
 	return (
 		<>
-			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.img} alt='movie'></img>
+			{props.movies.map((movie, id) => (
+				<div className='image-container d-flex justify-content-start m-3' key={id}>
+					<img src={movie.imgsrc} alt='movie'></img>
+					<div className='overlay d-flex align-items-center justify-content-center'>
+						{movie.name}
+					</div>
 				</div>
 			))}
 		</>
